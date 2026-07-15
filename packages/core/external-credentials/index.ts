68ebf44d2ebe6e6728e5d5fba06f5341f0c7ae20
yunfei <yunfeihu@tencent.com>
2026-07-15 13:43:14 +0800

    refactor(credentials): remove forwarding package barrel

--- packages/core/external-credentials/index.ts
+++
@@ -1,8 +0,0 @@
-export {
-  externalCredentialProfileKeys,
-  externalCredentialProfilesOptions,
-  useCreateExternalCredentialProfile,
-  useDeleteExternalCredentialProfile,
-  useTestExternalCredentialProfile,
-  useUpdateExternalCredentialProfile,
-} from "./queries";
